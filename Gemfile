6928008349d953a46ca67744394530592f20b681
Vitaliy Zakharov <vz@ss18.local>
2013-08-25 04:24:36 -0400

    prototyping UI

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ gem 'sinatra'
 # gem "sqlite3"
 gem "activerecord"
 gem "sinatra-activerecord"
+gem "sass"
  
 group :development do
   gem "shotgun"
--- fatcat.rb
+++ fatcat.rb
@@ -1,6 +1,25 @@
 require 'rubygems'
 require 'sinatra'
+require 'sass'
 
-get '/' do
-  File.read(File.join('public', 'index.html'))
+set :public_folder, 'public'
+class SassEngine < Sinatra::Base
+  set :views,   File.dirname(__FILE__)    + '/assets/sass'
+
+  get '/css/*.css' do
+    filename = params[:splat].first
+    sass filename.to_sym
+  end
 end
+# class CoffeeHandler < Sinatra::Base
+    # set :views, File.dirname(__FILE__) + '/assets/coffee'
+    # get "/js/*.js" do
+        # filename = params[:splat].first
+        # coffee filename.to_sym
+    # end
+# end
+use SassEngine
+get '/' do
+  redirect 'index.html'
+end
+
