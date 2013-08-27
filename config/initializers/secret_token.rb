66d9ede3f49ecdf8caeee5fa40cf82c081732f3e
Vitaliy Zakharov <vz@ss18.local>
2013-08-26 22:36:56 -0400

    now using rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fclite::Application.config.secret_key_base = 'd4771e5568d36af82ef329c1133a63df94f74f985ddef0c22529d2536bf1ea12e9fbecbea4ae53a57e4424890e16db6fa17e81e829772c110c9dc66aff6dc45c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fclite::Application.config.session_store :cookie_store, key: '_fclite_session'
